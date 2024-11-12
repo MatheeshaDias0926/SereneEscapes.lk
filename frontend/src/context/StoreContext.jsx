45587b5ee7b8064a6b446ade5e2b5551a1656b07
Matheesha Dias <matheeshadias@gmail.com>
2024-11-13 01:54:31 +0530

    ede
    ded

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -1,28 +1,12 @@
-import React, { createContext, useEffect, useState } from "react";
-import axios from "axios";
+import React, { createContext, useState } from "react";
 
 export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
-  const url = "http://localhost:4000";
-  const [disease_list, setDiseaseList] = useState([]);
-
-  const fetchDiseaseList = async () => {
-    try {
-      const response = await axios.get(url + "/api/disease/list");
-      setDiseaseList(response.data.data); // Corrected function usage here
-    } catch (error) {
-      console.error("Failed to fetch disease list:", error);
-    }
-  };
-
-  useEffect(() => {
-    fetchDiseaseList();
-  }, []);
+  const [disease_list] = useState([]);
 
   const contextValue = {
     disease_list,
-    url,
   };
 
   return (
