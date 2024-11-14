d4fa5885ad594dbe5757305905eaeb030c35295f
Matheesha Dias <matheeshadias@gmail.com>
2024-11-14 15:05:40 +0530

    dede
    dede

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  base: '/SereneEscapes.lk/',  // Replace 'your-repo-name' with the actual repository name
+});
